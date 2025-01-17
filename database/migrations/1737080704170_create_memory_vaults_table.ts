fe3ff1d8db8436e59f358e449296f7948076eca5
Jacob <jacobnburns@gmail.com>
2025-01-16 18:42:29 -0800

    added sqlite schema

---
+++ database/migrations/1737080704170_create_memory_vaults_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'memory_vaults'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id') // Primary key
+      table.string('title').nullable() // Nullable title
+      table.string('location').nullable() // Nullable location
+      table.date('date').notNullable() // Memory date
+      table.text('memory').notNullable() // Required memory content
+      table.json('tags').nullable() // Tags as JSON (nullable)
+      table.json('attachment').nullable() // Attachments as JSON (nullable)
+
+      table.timestamps(true, true) 
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
